c8a95afdff32f0e93c8b9dd45dbd541a799dada0
Ariangelo <ariangelo@gmail.com>
2018-08-11 19:29:57 -0300

    Requerimento

---
+++ model.py
@@ -0,0 +1,22 @@
+from mongoengine import *
+
+class User(Document):
+  login = StringField(max_length=30)
+  password = StringField(max_length=30)
+  level = StringField()
+  email = EmailField(max_length=30)
+  name = StringField(max_length=30)
+  
+class Device(Document):
+  mac = StringField()
+  description = StringField()
+  enabled = BooleanField()
+  status = DictField()
+  type = IntField()
+  date = DateTimeField()
+  
+class Scene(Document):
+  description = StringField()
+  enabled = BooleanField()
+  devices = StringField()
+  _class = StringField()
